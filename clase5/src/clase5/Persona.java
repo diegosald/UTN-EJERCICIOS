39bdbbb9b3e0d19461ebf63edbc2b815e01234ef
diegosald <diegosalday@gmail.com>
2023-03-20 15:44:40 -0300

    nueva clase

---
+++ clase5/src/clase5/Persona.java
@@ -0,0 +1,12 @@
+package clase5;
+
+import java.time.LocalDateTime;
+
+public class Persona {
+
+	private String nombre;
+	private String apellido;
+	private LocalDateTime fechaDeNacimiento;
+	
+	
+}
