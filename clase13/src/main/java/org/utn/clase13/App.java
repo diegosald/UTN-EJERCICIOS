7d4e4651789d17eeae02c6f21c05fed46802294b
diegosald <diegosalday@gmail.com>
2023-04-11 23:33:06 -0300

    nuevo

--- clase13/src/main/java/org/utn/clase13/App.java
+++ clase13/src/main/java/org/utn/clase13/App.java
@@ -6,10 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
