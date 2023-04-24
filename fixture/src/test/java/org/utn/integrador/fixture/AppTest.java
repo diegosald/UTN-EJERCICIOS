8a8862477f72ddffec2191910ab193c092670219
diegosald <diegosalday@gmail.com>
2023-04-24 08:32:23 -0300

    ultimo

--- fixture/src/test/java/org/utn/integrador/fixture/AppTest.java
+++ fixture/src/test/java/org/utn/integrador/fixture/AppTest.java
@@ -34,7 +34,6 @@ public class AppTest
         Equipo equipo1 = new Equipo("ARG");
         Equipo equipo2 = new Equipo("BRA");
     	Partido partidoTest = new Partido(0,0,equipo1,equipo2,2,1);
-    	Participante participanteTest = new Participante(1,"Persona1");
     	Pronostico pronosticoTest = new Pronostico(0,0,partidoTest,equipo1,ResultadoEnum.GANO);
     	System.out.println( pronosticoTest.puntos());
         int puntoEsperado= Integer.parseInt(pronosticoTest.getPuntosacertado());
